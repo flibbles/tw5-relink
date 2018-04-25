a0f8783f2de7fe3891cd5b2639fbbb9d0918b4c7
Cameron Fischer <fischer.cameron@gmail.com>
2018-04-25 14:05:47 -0400

    Using chai-helpers boot method

--- test/tiddlywiki.js
+++ test/tiddlywiki.js
@@ -2,36 +2,12 @@
 This loads all the tiddlywiki tests by having tiddlywiki run their modules.
 \*/
 
-var prefix = require("tiddlywiki/boot/bootprefix.js").bootprefix();
-prefix.boot.argv = ["--version"];
-var $tw = require("tiddlywiki").TiddlyWiki(prefix);
+var chaiHelpers = require('chai-helpers');
 
-function loadDirectory(path) {
-	var tiddlerFiles = $tw.loadTiddlersFromPath(path);
-	$tw.utils.each(tiddlerFiles, function(tiddlerFile) {
-		$tw.wiki.addTiddlers(tiddlerFile.tiddlers);
-	});
-}
-
-function loadPlugin(path) {
-	var plugin = $tw.loadPluginFolder(path);
-	$tw.wiki.addTiddler(plugin);
-};
-
-var oldLoader = $tw.loadTiddlersNode;
-$tw.loadTiddlersNode = function() {
-	oldLoader.call($tw);
-	loadPlugin("./plugins/flibbles/relink");
-	loadDirectory("./test/tiddlers");
-};
-
-$tw.boot.boot();
-$tw.describe = describe;
-$tw.it = it;
-$tw.before = before;
-$tw.after = after;
-$tw.beforeEach = beforeEach;
-$tw.afterEach = afterEach;
+var $tw = chaiHelpers.boot(function(loader) {
+	loader.loadPlugin("./plugins/flibbles/relink");
+	loader.loadDirectory("./test/tiddlers");
+});
 
 // This line here loads all the tests into Mocha
 $tw.modules.applyMethods("test.relink");
