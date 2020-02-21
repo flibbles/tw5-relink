f77332f1ddc26421f28c7bf11a0df4ca445c4188
Cameron Fischer <fischer.cameron@gmail.com>
2020-02-21 17:40:11 -0500

    Completed text/x-tiddler-filter handling

--- tiddlers/test/filtertext.js
+++ tiddlers/test/filtertext.js
@@ -11,20 +11,30 @@ describe("filtertext", function() {
 function testDefault(text, expected, options) {
 	[text, expected, options] = utils.prepArgs(text, expected, options);
 	var type = options.type || "text/vnd.tiddlywiki";
+	var failCount = options.fails || 0;
 	var results = utils.relink({text: text, type: type}, Object.assign({target: "$:/DefaultTiddlers"}, options));
 	expect(results.tiddler.fields.text).toEqual(expected);
+	expect(results.fails.length).toEqual(failCount, "Incorrect number of failures");
 };
 
 it('manages $:/DefaultTiddlers', function() {
-	var options = {target: "$:/DefaultTiddlers"};
-	testDefault("[[from here]]", options);
-	testDefault("[tag[from here]]", options);
-	testDefault("from", "to", Object.assign({from: "from", to: "to"}, options));
+	testDefault("[[from here]]");
+	testDefault("[tag[from here]]");
+	testDefault("from", "to", {from: "from", to: "to"});
 });
 
 it('manages tiddlers with text/x-tiddler-filter type', function() {
-	var options = {type: "text/x-tiddler-filter"};
+	var options = {type: "text/x-tiddler-filter", target: "something else"};
 	testDefault("[tag[from here]]", options);
+});
+
+it('preserves whitespace', function() {
+	testDefault("[[something else]]\n[[from here]]\n[tag[stuff]]");
+});
+
+it('throws error in case of failure', function() {
+	var text = "[tag[from here]]";
+	testDefault(text, text, {to: "brackets[[in]]title", fails: 1});
 });
 
 });
