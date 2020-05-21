b73a7821a2a658969ed85f5edb50d432320f2cd7
Cameron Fischer <fischer.cameron@gmail.com>
2020-05-21 18:29:27 -0400

    WikiRelinker handles relinking on its own

--- plugins/relink/js/fieldtypes/wikitext.js
+++ plugins/relink/js/fieldtypes/wikitext.js
@@ -38,8 +38,11 @@ function collectRules() {
 	return rules;
 }
 
-function WikiRelinker(text, title, toTitle, options) {
-	WikiParser.call(this, null, text, options);
+function WikiRelinker(text, title, fromTitle, toTitle, options) {
+	WikiParser.call(this, "text/vnd.tiddlywiki", text, options);
+	this.entry = new WikitextEntry();
+	this.builder = new Rebuilder(text);
+	this.options = options;
 	if (!this.relinkMethodsInjected) {
 		var rules = collectRules();
 		$tw.utils.each([this.pragmaRuleClasses, this.blockRuleClasses, this.inlineRuleClasses], function(classList) {
@@ -53,6 +56,7 @@ function WikiRelinker(text, title, toTitle, options) {
 		WikiRelinker.prototype.relinkMethodsInjected = true;
 	}
 	this.title = title;
+	this.fromTitle = fromTitle;
 	this.toTitle = toTitle;
 	this.inlineRules = this.inlineRules.concat(this.pragmaRules, this.blockRules);
 	if (options.extraRules) {
@@ -70,41 +74,43 @@ WikiRelinker.prototype.parsePragmas = function() {return []; };
 WikiRelinker.prototype.parseInlineRun = function() {};
 WikiRelinker.prototype.parseBlocks = function() {};
 
+WikiRelinker.prototype.relinkRule = function(ruleInfo) {
+	if (ruleInfo.rule.relink) {
+		var newEntry = ruleInfo.rule.relink(this.source, this.fromTitle, this.toTitle, this.options);
+		if (newEntry !== undefined) {
+			this.entry.add(newEntry);
+			if (newEntry.output) {
+				this.builder.add(newEntry.output, ruleInfo.matchIndex, this.pos);
+			}
+		}
+	} else {
+		if (ruleInfo.rule.matchRegExp !== undefined) {
+			this.pos = ruleInfo.rule.matchRegExp.lastIndex;
+		} else {
+			// We can't easily determine the end of this
+			// rule match. We'll "parse" it so that
+			// parser.pos gets updated, but we throw away
+			// the results.
+			ruleInfo.rule.parse();
+		}
+	}
+};
+
 exports.relink = function(wikitext, fromTitle, toTitle, options) {
 	// fromTitle doesn't even show up plaintext. No relinking to do.
 	if (!options.settings.survey(wikitext, fromTitle, options)) {
 		return undefined;
 	}
-	var builder = new Rebuilder(wikitext),
-		parser = new WikiRelinker(wikitext, options.currentTiddler, toTitle, options),
-		matchingRule,
-		entry = new WikitextEntry(),
+	var matchingRule,
 		newOptions = $tw.utils.extend({}, options);
 	newOptions.settings = options.settings.createChildLibrary(options.currentTiddler);
+	var parser = new WikiRelinker(wikitext, options.currentTiddler, fromTitle, toTitle, newOptions);
 	while (matchingRule = parser.findNextMatch(parser.relinkRules, parser.pos)) {
-		if (matchingRule.rule.relink) {
-			var newEntry = matchingRule.rule.relink(wikitext, fromTitle, toTitle, newOptions);
-			if (newEntry !== undefined) {
-				entry.add(newEntry);
-				if (newEntry.output) {
-					builder.add(newEntry.output, matchingRule.matchIndex, parser.pos);
-				}
-			}
-		} else {
-			if (matchingRule.rule.matchRegExp !== undefined) {
-				parser.pos = matchingRule.rule.matchRegExp.lastIndex;
-			} else {
-				// We can't easily determine the end of this
-				// rule match. We'll "parse" it so that
-				// parser.pos gets updated, but we throw away
-				// the results.
-				matchingRule.rule.parse();
-			}
-		}
+		parser.relinkRule(matchingRule);
 	}
-	if (entry.children.length > 0) {
-		entry.output = builder.results();
-		return entry;
+	if (parser.entry.children.length > 0) {
+		parser.entry.output = parser.builder.results();
+		return parser.entry;
 	}
 	return undefined;
 };
