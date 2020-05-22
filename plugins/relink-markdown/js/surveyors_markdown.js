e67080c78b0715671c218a585a7ae19a28f6968d
Cameron Fischer <fischer.cameron@gmail.com>
2020-05-22 16:46:31 -0400

    Added optimization to surveyor

--- plugins/relink-markdown/js/surveyors_markdown.js
+++ plugins/relink-markdown/js/surveyors_markdown.js
@@ -16,9 +16,30 @@ function markdownEnabled(options) {
 		var test = $tw.wiki.renderText("text/html", "text/x-markdown", "[test](#test)");
 		_enabled = test.indexOf("<a") >= 0;
 	}
-	return _enabled;
+	return _enabled && options.type === "text/x-markdown";
 };
 
+var decoded, encoded;
+
 exports.survey = function(text, fromTitle, options) {
-	return markdownEnabled() && text.indexOf(utils.encodeLink(fromTitle)) >= 0;
+	if (markdownEnabled(options)) {
+		if (fromTitle !== decoded) {
+			decoded = fromTitle;
+			// Because survey can be called thousands of times with the same
+			// fromTitle, we have a one-value cache for holding that value's
+			// encoded version.
+			// It's an optimization, but that's what the surveyors are for.
+			// Encoding is expensive.
+			encoded = utils.encodeLink(decoded);
+			if (encoded === decoded) {
+				// The two are the same. So the raw surveyor will find it.
+				// No need for this one.
+				encoded = null;
+			}
+		}
+		if (encoded !== null) {
+			return text.indexOf(encoded) >= 0;
+		}
+	}
+	return false;
 };
