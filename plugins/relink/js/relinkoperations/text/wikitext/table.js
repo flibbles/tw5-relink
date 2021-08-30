2ecd2f924a13de4b34fb189648a8c46c55e06b5f
Cameron Fischer <fischer.cameron@gmail.com>
2021-08-30 11:57:14 -0400

    support for tables

---
+++ plugins/relink/js/relinkoperations/text/wikitext/table.js
@@ -0,0 +1,175 @@
+/*\
+module-type: relinkwikitextrule
+
+Handles tables. Or rather handles the cells inside the tables, since tables
+themselves aren't relinked.
+
+\*/
+
+var Rebuilder = require("$:/plugins/flibbles/relink/js/utils/rebuilder");
+
+exports.name = "table";
+
+exports.types = {block: true};
+
+exports.report = function(text, callback, options) {
+	var rowRegExp = /^\|([^\n]*)\|([fhck]?)\r?(?:\n|$)/mg,
+		rowTermRegExp = /(\|(?:[fhck]?)\r?(?:\n|$))/mg;
+	// Match the row
+	rowRegExp.lastIndex = this.parser.pos;
+	var rowMatch = rowRegExp.exec(this.parser.source);
+	while(rowMatch && rowMatch.index === this.parser.pos) {
+		var rowType = rowMatch[2];
+		// Check if it is a class assignment
+		if(rowType === "k") {
+			this.parser.pos = rowMatch.index + rowMatch[0].length;
+		} else if(rowType === "c") {
+			// Is this a caption row?
+			// If so, move past the opening `|` of the row
+			this.parser.pos++;
+			// Parse the caption
+			var oldCallback = this.parser.callback;
+			this.parser.callback = function(title, blurb) {
+				callback(title, '|' + blurb + '|c');
+			};
+			try {
+				this.parser.parseInlineRun(rowTermRegExp,{eatTerminator: true});
+			} finally {
+				this.parser.callback = oldCallback;
+			}
+		} else {
+			// Process the row
+			processRow.call(this, rowType, callback);
+			this.parser.pos = rowMatch.index + rowMatch[0].length;
+		}
+		rowMatch = rowRegExp.exec(this.parser.source);
+	}
+};
+
+exports.relink = function(text, fromTitle, toTitle, options) {
+	var rowRegExp = /^\|([^\n]*)\|([fhck]?)\r?(?:\n|$)/mg,
+		rowTermRegExp = /(\|(?:[fhck]?)\r?(?:\n|$))/mg,
+		builder = new Rebuilder(text, this.parser.pos),
+		impossible = false,
+		output,
+		entry;
+	// Match the row
+	rowRegExp.lastIndex = this.parser.pos;
+	var rowMatch = rowRegExp.exec(this.parser.source);
+	while(rowMatch && rowMatch.index === this.parser.pos) {
+		var rowType = rowMatch[2];
+		// Check if it is a class assignment
+		if(rowType === "k") {
+			this.parser.pos = rowMatch.index + rowMatch[0].length;
+		} else {
+			// Is this a caption row?
+			if(rowType === "c") {
+				// If so, move past the opening `|` of the row
+				this.parser.pos++;
+				// Parse the caption
+				output = this.parser.parseInlineRun(rowTermRegExp,{eatTerminator: true});
+			} else {
+				// Process the row
+				output = processRow.call(this);
+				this.parser.pos = rowMatch.index + rowMatch[0].length;
+			}
+			if (output.length > 0) {
+				for (var i = 0; i < output.length; i++) {
+					var o = output[i];
+					if (o.output) {
+						builder.add(o.output, o.start, o.end);
+					}
+					if (o.impossible) {
+						impossible = true;
+					}
+				}
+			}
+		}
+		rowMatch = rowRegExp.exec(this.parser.source);
+	}
+	if (builder.changed() || impossible) {
+		var entry = {}
+		entry.output = builder.results(this.parser.pos);
+		if (impossible) {
+			entry.impossible = true;
+		}
+	}
+	return entry;
+};
+
+var processRow = function(rowType, callback) {
+	var cellRegExp = /(?:\|([^\n\|]*)\|)|(\|[fhck]?\r?(?:\n|$))/mg,
+		cellTermRegExp = /((?:\x20*)\|)/mg,
+		children = [];
+	// Match a single cell
+	cellRegExp.lastIndex = this.parser.pos;
+	var cellMatch = cellRegExp.exec(this.parser.source);
+	while(cellMatch && cellMatch.index === this.parser.pos) {
+		if(cellMatch[2]) {
+			// End of row
+			this.parser.pos = cellRegExp.lastIndex - 1;
+			break;
+		}
+		switch (cellMatch[1]) {
+		case '~':
+		case '>':
+		case '<':
+			// Move to just before the `|` terminating the cell
+			this.parser.pos = cellRegExp.lastIndex - 1;
+			break;
+		default:
+			// For ordinary cells, step beyond the opening `|`
+			this.parser.pos++;
+			// Look for a space at the start of the cell
+			var spaceLeft = false;
+			var prefix = '|';
+			var suffix = '|';
+			if(this.parser.source.substr(this.parser.pos).search(/^\^([^\^]|\^\^)/) === 0) {
+				prefix += '^';
+				this.parser.pos++;
+			} else if(this.parser.source.substr(this.parser.pos).search(/^,([^,]|,,)/) === 0) {
+				prefix += ',';
+				this.parser.pos++;
+			}
+			var chr = this.parser.source.substr(this.parser.pos,1);
+			while(chr === " ") {
+				spaceLeft = true;
+				this.parser.pos++;
+				chr = this.parser.source.substr(this.parser.pos,1);
+			}
+			if (spaceLeft) {
+				prefix += ' ';
+			}
+			// Check whether this is a heading cell
+			if(chr === "!") {
+				this.parser.pos++;
+				prefix += '!';
+			}
+			// Parse the cell
+			var oldCallback = this.parser.callback;
+			var reports = [];
+			this.parser.callback = function(title, blurb) {
+				reports.push(title, blurb);
+			};
+			try {
+				var output = this.parser.parseInlineRun(cellTermRegExp,{eatTerminator: true});
+				if (output.length > 0) {
+					children.push(output[0]);
+				}
+				if(this.parser.source.substr(this.parser.pos - 2,1) === " ") { // spaceRight
+					suffix = ' |';
+				}
+				for (var i = 0; i < reports.length; i += 2) {
+					callback(reports[i], prefix + reports[i+1] + suffix + rowType);
+				}
+			} finally {
+				this.parser.callback = oldCallback;
+			}
+			// Move back to the closing `|`
+			this.parser.pos--;
+		}
+		cellRegExp.lastIndex = this.parser.pos;
+		cellMatch = cellRegExp.exec(this.parser.source);
+	}
+	return children;
+};
---
+++ tiddlers/test/table.js
@@ -0,0 +1,141 @@
+/*\
+
+Tests transcludes.
+
+\*/
+
+var utils = require("test/utils");
+
+function testText(text, expected, report, options) {
+	options = Object.assign({from: 'from here', to: 'to there'}, options);
+	const wiki = options.wiki || new $tw.Wiki();
+	if (expected === true) {
+		expected = text.split(options.from).join(options.to);
+	} else if (expected === false) {
+		expected = text;
+	}
+	wiki.addTiddlers([
+		{title: 'test', text: text},
+		utils.operatorConf("title")]);
+	expect(utils.getReport('test', wiki)[options.from]).toEqual(report);
+	wiki.renameTiddler(options.from, options.to, options);
+	expect(utils.getText('test', wiki)).toEqual(expected);
+};
+
+describe("table", function() {
+
+beforeEach(function() {
+	spyOn(console, 'log');
+});
+
+it('works', function() {
+	// Issue #32: transcludes in tables didn't work.
+	testText("|{{from here}}|", true, ['|{{}}|']);
+	testText("|{{from here}}|\n", true, ['|{{}}|']);
+	testText("|{{from here}}|\r\n", true, ['|{{}}|']);
+	testText("|{{from here}}|\nstuff", true, ['|{{}}|']);
+	testText("|{{from here}}|\r\nstuff", true, ['|{{}}|']);
+	testText("|{{from here||template}}|", true, ['|{{||template}}|']);
+});
+
+it('headers', function() {
+	testText("|[[from here]]|h\n|something|", true, ['|[[from here]]|h']);
+	testText("|first|\n|[[from here]]|h\n|else|", true, ['|[[from here]]|h']);
+
+	testText("|![[from here]]|A|B|", true, ['|![[from here]]|']);
+});
+
+it('footers', function() {
+	testText("|[[from here]]|f\n|something|", true, ['|[[from here]]|f']);
+	testText("|first|\n|[[from here]]|f\n|else|", true, ['|[[from here]]|f']);
+});
+
+it('classes', function() {
+	// Classes are taken as plaintext. There can't be any links in them.
+	testText("|[[from here]]|k\n|[[from here]]|",
+	         "|[[from here]]|k\n|[[to there]]|", ['|[[from here]]|']);
+	testText("|first|\n|{{from here}}|k\n|{{from here}}|",
+	         "|first|\n|{{from here}}|k\n|{{to there}}|", ['|{{}}|']);
+});
+
+it('captions', function() {
+	// Classes are taken as plaintext. There can't be any links in them.
+	testText("|[[from here]] | blah|c\n|stuff|", true, ['|[[from here]]|c']);
+	testText("|first|\n|a{{from here}}b|c\n| stuff|", true, ['|{{}}|c']);
+	// Doesn't process like a cell
+	testText("|first|\n|! {{from here}}|c", true, ['|{{}}|c']);
+	// Non relinkable captions don't crash
+	testText("|{{from here}}|\n|! no relink |c", true, ['|{{}}|']);
+});
+
+it('alignment', function() {
+	testText("|{{from here}}|", true, ['|{{}}|']);
+	testText("|{{from here}} |", true, ['|{{}} |']);
+	testText("| {{from here}}|", true, ['| {{}}|']);
+	testText("| {{from here}} |", true, ['| {{}} |']);
+	testText("|    {{from here}}    |", true, ['| {{}} |']);
+	testText("|^{{from here}}|", true, ['|^{{}}|']);
+	testText("|,{{from here}}|", true, ['|,{{}}|']);
+	testText("|^ {{from here}}|", true, ['|^ {{}}|']);
+	testText("|, {{from here}}|", true, ['|, {{}}|']);
+	testText("|^^{{from here}}^^|", true, ['|{{}}|']);
+	testText("|,,{{from here}},,|", true, ['|{{}}|']);
+	testText("|^^^{{from here}}^^|", true, ['|^{{}}|']);
+	testText("|,,,{{from here}},,|", true, ['|,{{}}|']);
+	testText("|^ ^^{{from here}}^^|", true, ['|^ {{}}|']);
+	testText("|, ,,{{from here}},,|", true, ['|, {{}}|']);
+	testText("|^!{{from here}} |", true, ['|^!{{}} |']);
+	// Tiddlywiki acts weird here, but the space comes before !
+	testText("|!{{from here}}|", true, ['|!{{}}|']);
+	testText("|! {{from here}}|", true, ['|!{{}}|']);
+	testText("| !{{from here}} |", true, ['| !{{}} |']);
+	testText("|,!{{from here}}|", true, ['|,!{{}}|']);
+	testText("|^ !{{from here}}|", true, ['|^ !{{}}|']);
+
+	// Cases which are probably user error
+	testText("|!^{{from here}}|", true, ['|!{{}}|']);
+	testText("|!, {{from here}}|", true, ['|!{{}}|']);
+});
+
+it('cell merges still parse', function() {
+	testText(`
+|Cell1 |Cell2 |Cell3 |Cell4 |
+|Cell5 |[[from]] |[[from]] |<|
+|Cell5 |~|Cell7 |Cell8 |
+|>|[[from]] |Cell10 |Cell11 |`, true,
+	['|[[from]] |', '|[[from]] |', '|[[from]] |'],
+	{from: 'from', to: 'to'});
+});
+
+it('strange cell merges', function() {
+	// Left merge when already on left
+	testText('| A | B |\n|<| {{from here}} |', true, ['| {{}} |']);
+	testText('| A |>|\n| B | {{from here}} |', true, ['| {{}} |']);
+	testText('|~| {{from here}} |\n| B | C |', true, ['| {{}} |']);
+});
+
+it('handles errors', function() {
+	utils.spyFailures(spyOn);
+	testText("| <$text text={{from here}} /> | [[from here]] |",
+	         "| <$text text={{from here}} /> | [[to!!there]] |",
+	         ['| <$text text={{}} /> |', '| [[from here]] |'],
+	         {to: "to!!there"});
+	expect(utils.failures).toHaveBeenCalledTimes(1);
+	utils.failures.calls.reset();
+	testText("| test |<$text text={{from here}} /> [[from here]]|c",
+	         "| test |<$text text={{from here}} /> [[to!!there]]|c",
+	         ['|<$text text={{}} />|c', '|[[from here]]|c'],
+	         {to: "to!!there"});
+	expect(utils.failures).toHaveBeenCalledTimes(1);
+});
+
+it('unpretty', function() {
+	var tricky = "bad' titles]]\"";
+	const wiki = new $tw.Wiki();
+	wiki.addTiddler(utils.attrConf('$link', 'to'));
+	testText('| test |[[from here]]|',
+	         utils.placeholder(1, tricky)+'| test |<$link to=<<relink-1>>/>|',
+	         ['|[[from here]]|'], {to: tricky, wiki: wiki});
+});
+
+});
