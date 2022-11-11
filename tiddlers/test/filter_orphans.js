a050f1fb7b578011db1488468d6d21ce17157848
Cameron Fischer <fischer.cameron@gmail.com>
2022-11-11 10:09:40 -0500

    Some preliminary tests for different tiddler types

--- tiddlers/test/filter_orphans.js
+++ tiddlers/test/filter_orphans.js
@@ -26,5 +26,17 @@ it('works with backup indexer', function() {
 	expect(wiki.filterTiddlers('[relink:orphans[]]')).toEqual(['A', 'B']);
 });
 
+// Issue #43
+it('can find images', function() {
+	var wiki = new $tw.Wiki();
+	wiki.addTiddler({title: "image.svg", type: "image/svg+xml", text: '<?xml version="1.0" encoding="UTF-8" standalone="no"?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8" version="1.1"><circle r="4" cx="4" cy="4" /></svg>'});
+	expect(wiki.filterTiddlers("[relink:orphans[]!is[system]is[image]]")).toEqual(['image.svg']);
+
+	wiki = new $tw.Wiki();
+	wiki.addTiddler({title: "image.png", type: "image/png", text: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII='});
+	expect(wiki.filterTiddlers("[relink:orphans[]!is[system]is[image]]")).toEqual(['image.png']);
+
+});
+
 });
 
