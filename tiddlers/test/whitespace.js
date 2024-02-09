e6d7e16979d62b300d0de7f8db920008d54665e2
Cameron Fischer <fischer.cameron@gmail.com>
2024-02-08 21:02:57 -0500

    Explicit tests for whitespace pragma

---
+++ tiddlers/test/whitespace.js
@@ -0,0 +1,31 @@
+/*\
+
+Tests whitespace pragma.
+
+\*/
+
+describe('whitespace', function() {
+
+var utils = require("test/utils");
+
+beforeEach(function() {
+	spyOn(console, 'log');
+});
+
+function test(text) {
+	const wiki = new $tw.Wiki();
+	wiki.addTiddler({title: 'test', text: text});
+	wiki.addTiddler(utils.operatorConf('tag'));
+	expect(utils.getReport('test', wiki)['from here']).toEqual(['\\function test() [tag[]]']);
+	wiki.renameTiddler('from here', 'to there', {wiki: wiki});
+	expect(utils.getText('test', wiki)).toEqual(text.split('from here').join('to there'));
+};
+
+it('does not interfere with later pragma', function() {
+	test('\\whitespace trim\n\\function test() [tag[from here]]\n');
+	test('\\whitespace trim\n\n\n\\function test() [tag[from here]]\n');
+	test('\\whitespace trim\r\n\r\n\\function test() [tag[from here]]\n');
+	test('\\whitespace trim\n    \\function test() [tag[from here]]\n');
+});
+
+});
