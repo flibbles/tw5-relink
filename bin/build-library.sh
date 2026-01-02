42fe770a3bd3e954b195aa9dc9363e4ac645b56e
Cameron Fischer <fischer.cameron@gmail.com>
2026-01-02 00:11:48 -0500

    Created script to create library from repository plugins
    Instead of from whatever relink plugins
    are available on TIDDLYWIKI_PLUGIN_PATH

---
+++ bin/build-library.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Builds the libary by setting the required environment necessary
+
+set -e
+
+echo "Creating artificial library environment"
+
+SCRIPT_PATH="$(dirname "$0")"
+ROOT_PATH="$(realpath "${SCRIPT_PATH}/..")"
+OUTPUT_PATH="${ROOT_PATH}/output/tmp"
+SYMLINK="${OUTPUT_PATH}/flibbles"
+
+# Libraries can only be built from the TIDDLYWIKI_PLUGIN_PATH env
+# Which expects plugins to available in (author)/(plugin)/plugin.info format.
+# Our repository is not that way, so we need to fake it for a second.
+mkdir -p "${OUTPUT_PATH}"
+ln -s "${ROOT_PATH}/plugins" "${SYMLINK}"
+
+# We want to be sure to clean this up by the time we're done
+trap "rm -Rf \"${OUTPUT_PATH}\"" EXIT
+
+# OUTPUT_PATH comes first, so it supercedes any globally installed relink
+TIDDLYWIKI_PLUGIN_PATH=${OUTPUT_PATH}:${TIDDLYWIKI_PLUGIN_PATH}
+
+# Now let's run the library with the path set correctly
+tiddlywiki --build library
