219d43de63ab5fe5f7edf717a92490829e47c115
Cameron Fischer <fischer.cameron@gmail.com>
2026-01-02 01:10:50 -0500

    More diagnostics

--- bin/build-library.sh
+++ bin/build-library.sh
@@ -21,11 +21,12 @@ ln -s "${ROOT_PATH}/plugins" "${SYMLINK}"
 trap "rm -Rf \"${OUTPUT_PATH}\"" EXIT
 
 # OUTPUT_PATH comes first, so it supercedes any globally installed relink
-TIDDLYWIKI_PLUGIN_PATH=${OUTPUT_PATH}:${TIDDLYWIKI_PLUGIN_PATH}
+TIDDLYWIKI_PLUGIN_PATH=${OUTPUT_PATH}
+#:${TIDDLYWIKI_PLUGIN_PATH}
 
 #Diagnostic stuff for now
 echo "TIDDLYWIKI_PLUGIN_PATH=${TIDDLYWIKI_PLUGIN_PATH}"
-ls -la "${ROOT_PATH}/plugins"
+ls -la "${SYMLINK}"
 
 # Now let's run the library with the path set correctly
 tiddlywiki --build library
